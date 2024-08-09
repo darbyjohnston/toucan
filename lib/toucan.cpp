e338f728864ca0dc2d00e74f9e788ea6ccebc4d1
Darby Johnston <darbyjohnston@yahoo.com>
2024-08-09 13:10:35 -0700

    Add license headers

--- lib/toucan.cpp
+++ lib/toucan.cpp
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2024 Darby Johnston
+// All rights reserved.
+
 #include "toucan.h"
 
 namespace toucan
--- lib/toucan.h
+++ lib/toucan.h
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2024 Darby Johnston
+// All rights reserved.
+
 #pragma once
 
 namespace toucan
