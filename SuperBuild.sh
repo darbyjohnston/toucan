b502dfb156e23aab323823c115f80bf1ca9f903a
Darby Johnston <darbyjohnston@yahoo.com>
2024-08-09 17:12:57 -0700

    Change job count

--- SuperBuild.sh
+++ SuperBuild.sh
@@ -5,7 +5,7 @@ set -x
 BUILD_TYPE=$1
 
 cmake -S toucan/cmake/SuperBuild -B SuperBuild-$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$PWD/install -DCMAKE_PREFIX_PATH=$PWD/install -DCMAKE_BUILD_TYPE=$BUILD_TYPE
-cmake --build SuperBuild-$BUILD_TYPE -j 1 --config $BUILD_TYPE
+cmake --build SuperBuild-$BUILD_TYPE -j 4 --config $BUILD_TYPE
 
 cmake -S toucan -B Build-$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$PWD/install -DCMAKE_PREFIX_PATH=$PWD/install -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 cmake --build Build-$BUILD_TYPE -j 4 --config $BUILD_TYPE
