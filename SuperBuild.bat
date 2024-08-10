4f00cd7ea3cbf225ffdda02e7cb25460501262c4
Darby Johnston <darbyjohnston@yahoo.com>
2024-08-09 17:35:35 -0700

    Build fix

--- SuperBuild.bat
+++ SuperBuild.bat
@@ -5,4 +5,4 @@ cmake --build SuperBuild-%BUILD_TYPE% -j 4 --config %BUILD_TYPE%
 
 cmake -S toucan -B Build-%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%CD%\install -DCMAKE_PREFIX_PATH=%CD%\install -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
 cmake --build Build-%BUILD_TYPE% -j 4 --config %BUILD_TYPE%
-cmake --build Build-%BUILD_TYPE% -j 4 --config %BUILD_TYPE% --target test
+cmake --build Build-%BUILD_TYPE% -j 4 --config %BUILD_TYPE% --target RUN_TESTS
