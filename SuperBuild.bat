a0bf72035bda31f8cb2a3b7e158e5809551cfa14
Darby Johnston <darbyjohnston@yahoo.com>
2024-08-14 18:48:09 -0700

    Add default

--- SuperBuild.bat
+++ SuperBuild.bat
@@ -1,4 +1,5 @@
 set BUILD_TYPE=%1
+IF "%BUILD_TYPE%"=="" set BUILD_TYPE=Release
 
 cmake -S toucan\cmake\SuperBuild -B superbuild-%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%CD%\install-%BUILD_TYPE% -DCMAKE_PREFIX_PATH=%CD%\install-%BUILD_TYPE% -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
 cmake --build superbuild-%BUILD_TYPE% -j 4 --config %BUILD_TYPE%
